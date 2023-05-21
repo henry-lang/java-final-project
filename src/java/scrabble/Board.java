f66a67c3fe77d1462fc37b5308ba53a5abf3b89e
henry-lang <henry.langmack@gmail.com>
2023-05-21 15:11:07 -0400

    scoring kind of works, but not with multuple or horizontal words.

--- src/java/scrabble/Board.java
+++ src/java/scrabble/Board.java
@@ -38,6 +38,10 @@ public class Board {
     private final Tile[][] tiles = new Tile[SIZE][SIZE];
 
     {
+//        tiles[1][1] = new Tile('c', 3, false);
+//        tiles[1][2] = new Tile('a', 1, false);
+//        tiles[1][4] = new Tile('t', 2, false);
+
         tiles[1][1] = new Tile('c', 3, false);
         tiles[2][1] = new Tile('a', 1, false);
         tiles[3][1] = new Tile('t', 2, false);
@@ -58,39 +62,77 @@ public class Board {
         return isPlaced;
     }
 
-    private boolean tilesInStraightLine() {
-        var row = -1;
-        var col = -1;
+    private static class TileLineInfo {
+        public final boolean inStraightLine;
+        public final boolean horizontal;
+        public final int startRow;
+        public final int startCol;
+        public final int endRow;
+        public final int endCol;
+
+        public static final TileLineInfo NOT_IN_STRAIGHT_LINE = new TileLineInfo(false, false, -1, -1, -1, -1);
+
+        public TileLineInfo(boolean inStraightLine, boolean horizontal, int startRow, int startCol, int endRow, int endCol) {
+            this.inStraightLine = inStraightLine;
+            this.horizontal = horizontal;
+            this.startRow = startRow;
+            this.startCol = startCol;
+            this.endRow = endRow;
+            this.endCol = endCol;
+        }
+    }
+
+    private TileLineInfo tilesInStraightLine() {
+        var startRow = -1;
+        var startCol = -1;
+        var lastRow = -1;
+        var lastCol = -1;
         var decidedDirection = false;
         var horizontal = false; // Only useful when decidedDirection is true
-        for(var i = 0; i < SIZE; i++) {
-            for(var j = 0; j < SIZE; j++) {
-                if(tiles[i][j] != null && !tiles[i][j].isFinalized()) {
-                    if(row != -1 && col != -1) {
+        for(var r = 0; r < SIZE; r++) {
+            for(var c = 0; c < SIZE; c++) {
+                if(tiles[r][c] != null && !tiles[r][c].isFinalized()) {
+                    if(lastRow != -1 && lastCol != -1) {
                         if(decidedDirection) {
-                            if((horizontal && i != row) || (!horizontal && j != col)) {
-                                return false;
+                            if((horizontal && r != lastRow) || (!horizontal && c != lastCol)) {
+                                return TileLineInfo.NOT_IN_STRAIGHT_LINE;
                             }
                         } else {
-                            if(row == i) {
+                            if(lastRow == r) {
                                 horizontal = true;
-                            } else if(col == j) {
+                            } else if(lastCol == c) {
                                 horizontal = false;
                             } else {
-                                // Tiles aren't in a straight line
-                                return false;
+                                return TileLineInfo.NOT_IN_STRAIGHT_LINE;
                             }
                         }
                         decidedDirection = true;
                     }
 
-                    row = i;
-                    col = j;
+                    if(startRow == -1 && startCol == -1) {
+                        startRow = r;
+                        startCol = c;
+                    }
+
+                    lastRow = r;
+                    lastCol = c;
                 }
             }
         }
 
-        return true;
+        return new TileLineInfo(true, horizontal, startRow, startCol, lastRow, lastCol);
+    }
+
+    private boolean checkForGapsInLine(TileLineInfo lineInfo) {
+        for(var r = lineInfo.startRow; r <= lineInfo.endRow; r++) {
+            for(var c = lineInfo.startCol; c <= lineInfo.endCol; c++) {
+                if(tiles[r][c] == null) {
+                    // There is a gap!
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public List<WordPlacementInfo> checkWordPlacement() {
@@ -101,10 +143,71 @@ public class Board {
             return wordPlacements;
         }
 
-        if(!tilesInStraightLine()) {
+        var lineInfo = tilesInStraightLine();
+        if(!lineInfo.inStraightLine) {
             wordPlacements.add(WordPlacementInfo.INVALID_STRAIGHT_LINE);
             return wordPlacements;
         }
+
+        if(checkForGapsInLine(lineInfo)) {
+            wordPlacements.add(WordPlacementInfo.INVALID_GAP_IN_LINE);
+            return wordPlacements;
+        }
+
+        if(lineInfo.horizontal) {
+            var col = lineInfo.startCol;
+            while(col > 0 && tiles[lineInfo.startRow][col] != null) {
+                col -= 1;
+            }
+        } else {
+            var col = lineInfo.startCol;
+
+            // Extend the line if there are tiles after or before the line
+            var startRow = lineInfo.startRow;
+            while(startRow > 0 && tiles[startRow - 1][col] != null) {
+                startRow -= 1;
+            }
+
+            var endRow = lineInfo.endRow;
+            while(endRow < SIZE - 1 && tiles[endRow + 1][col] != null) {
+                endRow += 1;
+            }
+
+            var pointValue = 0;
+            var wordMultiplier = 1;
+            var word = new StringBuilder();
+            for(var row = startRow; row <= endRow; row++) {
+                var tile = tiles[row][col];
+                var tileMultiplier = 1;
+                if(!tile.isFinalized()) {
+                    var multiplier = multipliers[row][col];
+                    switch(multiplier) {
+                        case DL: {
+                            tileMultiplier = 2;
+                            break;
+                        }
+                        case TL: {
+                            tileMultiplier = 3;
+                            break;
+                        }
+                        case DW: {
+                            wordMultiplier *= 2;
+                            break;
+                        }
+                        case TW: {
+                            wordMultiplier *= 3;
+                        }
+                    }
+                }
+                pointValue += tile.getValue() * tileMultiplier;
+                word.append(tiles[row][col].getLetter());
+            }
+
+            pointValue *= wordMultiplier;
+            wordPlacements.add(new WordPlacementInfo(true, pointValue, word.toString()));
+        }
+
+        System.out.println(lineInfo.horizontal + " " + lineInfo.startRow + " " + lineInfo.startCol + " " + lineInfo.endRow + " " + lineInfo.endCol);
 
         wordPlacements.sort(Comparator.comparingInt(a -> a.pointValue));
         return wordPlacements;
--- src/java/scrabble/WordPlacementInfo.java
+++ src/java/scrabble/WordPlacementInfo.java
@@ -3,6 +3,9 @@ package scrabble;
 public class WordPlacementInfo {
     public static final WordPlacementInfo INVALID_STRAIGHT_LINE = new WordPlacementInfo(false, 0, "Tiles must be in a straight line");
     public static final WordPlacementInfo INVALID_NO_TILES = new WordPlacementInfo(false, 0, "No tiles placed");
+    public static final WordPlacementInfo INVALID_GAP_IN_LINE = new WordPlacementInfo(false, 0, "Gaps between tiles are not allowed");
+    public static final WordPlacementInfo INVALID_CENTER_SQUARE = new WordPlacementInfo(false, 0, "First move must use center square");
+    public static final WordPlacementInfo INVALID_TOUCH_EXISTING = new WordPlacementInfo(false, 0, "Move must touch existing tiles");
     public final boolean isValid;
     public final int pointValue;
     public final String invalidReason;
